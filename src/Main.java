15ad2f5336df652fdffeb10eb2d0cccd3b65fb60
Liliya <liliya.penza@gmail.com>
2023-08-02 15:46:45 +0200

    added gitignore

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 18500;
+
+        int bonus = price / 20;
+
+        System.out.println("Бонус: " + bonus);
+
+    }
+}
